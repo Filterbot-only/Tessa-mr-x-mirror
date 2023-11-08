c1681da1b06e751668c2285f1795da6750e22b5d
Filterbot-only <147235633+Filterbot-only@users.noreply.github.com>
2023-11-08 21:02:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -14,7 +14,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '21953115'))
 API_HASH = environ.get('API_HASH', 'edfd34085e9ba51303155f75d77b09ae')
-BOT_TOKEN = environ.get('BOT_TOKEN', "6694807065:AAEJ7mUzgEFHOjMBqlhhC59HPfh2UxavSeA")
+BOT_TOKEN = environ.get('BOT_TOKEN', "6694807065:AAF0qyX0ARKEc88WDBzJ0gxEPfQ9KUHhBBQ")
 
 #Port
 PORT = environ.get("PORT", "8080")
